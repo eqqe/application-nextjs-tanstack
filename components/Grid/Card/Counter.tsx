06c0a1cd20ed1983947332e2746040b6eadefc03
Simon Rey <sfbrey+eqqe@gmail.com>
2024-06-20 11:08:58 +0200

    Add test

--- components/Grid/Card/Counter.tsx
+++ components/Grid/Card/Counter.tsx
@@ -1,9 +1,10 @@
 import { Type } from '@zenstackhq/runtime/models';
 import { getTypeHook } from '@/components/Grid/Table/getTypeHook';
+import { beautifyObjectName } from '@/components/ui/auto-form/utils';
 
 export function Counter({ type }: { type: Type }) {
     const typeHook = getTypeHook({ type });
     // @ts-expect-error
     const { data: count } = typeHook.useCount();
-    return <>{count}</>;
+    return <>{`${count} ${beautifyObjectName(type)}`}</>;
 }
