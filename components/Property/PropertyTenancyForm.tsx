5e517f8a30892ee7344d3d476d039e8ca51928d2
Simon Rey <sfbrey+eqqe@gmail.com>
2024-06-20 02:03:38 +0200

    Fix

--- components/Property/PropertyTenancyForm.tsx
+++ components/Property/PropertyTenancyForm.tsx
@@ -2,16 +2,31 @@ import { useCreatePropertyTenancy } from '@/zmodel/lib/hooks';
 import { toast } from 'react-toastify';
 import { AutoFormDialog } from '@/components/Form/AutoFormDialog';
 import { z } from 'zod';
-import { PropertyTenancyCreateScalarSchema } from '@zenstackhq/runtime/zod/models';
+import {
+    PropertyJointTenancyCreateScalarSchema,
+    PropertyTenancyByEntiretyCreateScalarSchema,
+    PropertyTenancyCreateScalarSchema,
+    PropertyTenancyInCommonCreateScalarSchema,
+} from '@zenstackhq/runtime/zod/models';
 
 export function PropertyTenancyForm() {
     const create = useCreatePropertyTenancy();
     return (
         <AutoFormDialog
-            formSchema={z.object({ tenancy: PropertyTenancyCreateScalarSchema })}
+            formSchema={z.object({
+                tenancy: PropertyTenancyCreateScalarSchema,
+                tenancyInCommon: PropertyTenancyInCommonCreateScalarSchema,
+            })}
             onSubmitData={async (data) => {
                 await create.mutateAsync({
-                    data: data.tenancy,
+                    data: {
+                        ...data.tenancy,
+                        ...(data.tenancy.tenancyType === 'InCommon' && {
+                            tenancyInCommon: {
+                                create: data.tenancyInCommon,
+                            },
+                        }),
+                    },
                 });
                 toast.success(`${data.tenancy.name} created successfully!`);
             }}
