5f452fa1d1e36145b651b6df106d4400068261c0
Simon Rey <sfbrey+eqqe@gmail.com>
2024-06-20 12:21:50 +0200

    Fix

--- e2e/applications/applications.spec.ts
+++ e2e/applications/applications.spec.ts
@@ -19,11 +19,12 @@ test('Should enable assets application, rollback, and update', async ({ page, ut
 
     await generateDemonstration();
     await page.getByText('Your essential data').click();
-    await expect(page.getByText(`${lenghtDemo} Property Tenancy`, { exact: true })).toBeVisible();
-    await expect(page.getByText(`${lenghtDemo * lenghtDemo} Lease`, { exact: true })).toBeVisible();
-    await expect(page.getByText(`${lenghtDemo} Property`, { exact: true })).toBeVisible();
-    await expect(page.getByText(`${lenghtDemo * lenghtDemo} Lease Tenant`, { exact: true })).toBeVisible();
+    await expect(page.getByText(`property tenants${lenghtDemo} Property Tenancy`)).toBeVisible();
+    await expect(page.getByText(`leases${lenghtDemo * lenghtDemo} Lease`)).toBeVisible();
+    await expect(page.getByText(`properties${lenghtDemo} Property`)).toBeVisible();
+    await expect(page.getByText(`lease tenants${lenghtDemo * lenghtDemo} Lease Tenant`)).toBeVisible();
 
+    await utils.openSettings();
     await page.getByText('Properties').click();
     await page.getByRole('tab', { name: 'Month' }).click();
     await expect(page.getByText(cityPlaywrightTest)).toBeVisible();
