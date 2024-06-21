616a60afa5e4ed8bce50d4be3fd0c035b8f7038c
Simon Rey <sfbrey+eqqe@gmail.com>
2024-06-22 01:51:05 +0200

    format date in tables

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { format } from 'date-fns';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export const dateFormat = (date: Date) => format(date, 'yyyy-MM-dd');
