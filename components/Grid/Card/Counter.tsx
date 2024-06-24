1b4eb09c233492029921f5fd64495872e9812077
Simon Rey <51708585+eqqe@users.noreply.github.com>
2024-06-24 12:59:52 +0200

    add types to try to make ts faster (#36)

--- components/Grid/Card/Counter.tsx
+++ components/Grid/Card/Counter.tsx
@@ -2,7 +2,6 @@ import { typeHooks } from '@/zmodel/lib/forms/typeHooks';
 import { Type } from '@prisma/client';
 export function Counter({ type }: { type: Type }) {
     const typeHook = typeHooks[type];
-    // @ts-expect-error
     const { data: count } = typeHook.useCount();
     return <>{count}</>;
 }
